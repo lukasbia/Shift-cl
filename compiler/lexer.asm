37b3470c8ad983c1d2d3b69c6b32c992f5bd0ba1
lukasbia <stodomingokylijahnver@gmail.com>
2026-09-10 20:13:43 +0800

    Update lexer.asm

--- compiler/lexer.asm
+++ compiler/lexer.asm
@@ -1,40 +1,33 @@
 section .data
-    msg_start   db "=== Assembly-C Bootstrap Compiler ===", 10, 0
-    msg_start_len equ $ - msg_start
-    
-    err_syntax  db "[COMPILER ERROR] Invalid keyword or syntax structure.", 10, 0
-    err_len     equ $ - err_syntax
+    kw_variable db "variable", 0
+    kw_constant db "constant", 0
+    kw_func     db "func", 0
+    kw_if       db "if", 0
+    kw_else     db "else", 0
+    kw_while    db "while", 0
+    kw_return   db "return", 0
 
 section .bss
-    token_buffer resb 2048
+    lexeme_buffer resb 64
+    token_stream  resb 4096
 
 section .text
-    global _start
+    global run_smart_lexer
 
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg_start
-    mov rdx, msg_start_len
-    syscall
-
-    mov rsi, sample_source_code
-
-    call parse_asmc_stream
-
-    mov rax, 60
-    xor rdi, rdi
-    syscall
-
-parse_asmc_stream:
+run_smart_lexer:
     push rbp
     mov rbp, rsp
+    push rbx
+    push r12
+    push r13
 
-.lex_loop:
-    mov al, byte [rsi]
-    
+    xor r12, r12
+    xor r13, r13
+
+.read_char:
+    mov al, byte [rsi + r12]
     cmp al, 0
-    je .lex_done
+    je .lexer_done
 
     cmp al, 60
     je .skip_comment
@@ -46,28 +39,111 @@ parse_asmc_stream:
     cmp al, 10
     je .next_char
 
-.next_char:
-    inc rsi
-    jmp .lex_loop
+    cmp al, 'a'
+    jl .check_symbol
+    cmp al, 'z'
+    jle .extract_identifier
+
+.extract_identifier:
+    xor rbx, rbx
+.collect_loop:
+    mov al, byte [rsi + r12]
+    cmp al, 'a'
+    jl .evaluate_identifier
+    cmp al, 'z'
+    jg .evaluate_identifier
+    mov byte [lexeme_buffer + rbx], al
+    inc rbx
+    inc r12
+    jmp .collect_loop
+
+.evaluate_identifier:
+    mov byte [lexeme_buffer + rbx], 0
+
+    mov rdi, lexeme_buffer
+    mov rsi, kw_variable
+    call str_cmp
+    cmp rax, 1
+    je .emit_variable_token
+
+    mov rdi, lexeme_buffer
+    mov rsi, kw_constant
+    call str_cmp
+    cmp rax, 1
+    je .emit_constant_token
+
+    mov rdi, lexeme_buffer
+    mov rsi, kw_func
+    call str_cmp
+    cmp rax, 1
+    je .emit_func_token
+
+    jmp .emit_identifier_token
+
+.emit_variable_token:
+    mov byte [token_stream + r13], 1
+    inc r13
+    jmp .read_char
+
+.emit_constant_token:
+    mov byte [token_stream + r13], 2
+    inc r13
+    jmp .read_char
+
+.emit_func_token:
+    mov byte [token_stream + r13], 3
+    inc r13
+    jmp .read_char
+
+.emit_identifier_token:
+    mov byte [token_stream + r13], 99
+    inc r13
+    jmp .read_char
+
+.check_symbol:
+    inc r12
+    jmp .read_char
 
 .skip_comment:
-    inc rsi
-    mov al, byte [rsi]
+    inc r12
+    mov al, byte [rsi + r12]
     cmp al, 0
-    je .lex_done
+    je .lexer_done
     cmp al, 10
     je .next_char
     jmp .skip_comment
 
-.lex_done:
-    pop rbp
+.next_char:
+    inc r12
+    jmp .read_char
+
+.str_cmp:
+    push rsi
+    push rdi
+.cmp_loop:
+    mov al, byte [rdi]
+    mov bl, byte [rsi]
+    cmp al, bl
+    jne .cmp_notequal
+    cmp al, 0
+    je .cmp_equal
+    inc rdi
+    inc rsi
+    jmp .cmp_loop
+.cmp_equal:
+    pop rdi
+    pop rsi
+    mov rax, 1
+    ret
+.cmp_notequal:
+    pop rdi
+    pop rsi
+    xor rax, rax
     ret
 
-section .data
-    sample_source_code:
-        db "< Test program source", 10
-        db "func main() {", 10
-        db "    variable balance: Int = 1250", 10
-        db "    print(string: \"Hello\", length: 5)", 10
-        db "    exit(code: 0)", 10
-        db "}", 10, 0
+.lexer_done:
+    pop r13
+    pop r12
+    pop rbx
+    pop rbp
+    ret
