dbfcfde6d6257525c0465a110d44a52c9433762e
lukasbia <stodomingokylijahnver@gmail.com>
2026-09-10 20:16:46 +0800

    Update generator.asm

--- compiler/generator.asm
+++ compiler/generator.asm
@@ -1,12 +1,29 @@
-global generate_code
 section .data
-    out_asm db "global _start", 10, "section .text", 10, "_start:", 10, "    mov rax, 60", 10, "    xor rdi, rdi", 10, "    syscall", 10
+    asm_header  db "global _start", 10, "section .text", 10, "_start:", 10, 0
+    asm_head_len equ $ - asm_header
+    asm_exit    db "    mov rax, 60", 10, "    xor rdi, rdi", 10, "    syscall", 10, 0
+    asm_exit_len equ $ - asm_exit
 
 section .text
-generate_code:
+    global run_generator
+
+run_generator:
+    push rbp
+    mov rbp, rsp
+    push rbx
+
     mov rax, 1
     mov rdi, 1
-    mov rsi, out_asm
-    mov rdx, 49
+    mov rsi, asm_header
+    mov rdx, asm_head_len
     syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, asm_exit
+    mov rdx, asm_exit_len
+    syscall
+
+    pop rbx
+    pop rbp
     ret
