66b24bd53748ba7308acbc0d6a051b174006cdd4
lukasbia <stodomingokylijahnver@gmail.com>
2026-09-10 19:18:16 +0800

    Update lexer.asm

--- compiler/lexer.asm
+++ compiler/lexer.asm
@@ -1,46 +1,34 @@
-global _start
-
-section .data
-    source_code   db "function", 0
-    kw_function   db "function", 0
-    msg_keyword   db "TOKEN: KEYWORD (function)", 10
-    msg_ident     db "TOKEN: IDENTIFIER", 10
+global lexer_scan
 
 section .text
-_start:
-    mov rsi, source_code
-    mov rdi, kw_function
-    xor rcx, rcx
-
-compare_loop:
+lexer_scan:
     mov al, byte [rsi + rcx]
-    mov bl, byte [rdi + rcx]
-
-    cmp al, bl
-    jne not_a_keyword
-
-    cmp al, 0
-    je found_keyword
-
+    test al, al
+    jz lexer_done
+    cmp al, ' '
+    je lexer_skip
+    cmp al, ':'
+    je lexer_colon
+    cmp al, ','
+    je lexer_comma
+    cmp al, '('
+    je lexer_open_paren
+    cmp al, ')'
+    je lexer_close_paren
+lexer_skip:
     inc rcx
-    jmp compare_loop
-
-found_keyword:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg_keyword
-    mov rdx, 27
-    syscall
-    jmp exit
-
-not_a_keyword:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg_ident
-    mov rdx, 17
-    syscall
-
-exit:
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+    jmp lexer_scan
+lexer_colon:
+    inc rcx
+    jmp lexer_scan
+lexer_comma:
+    inc rcx
+    jmp lexer_scan
+lexer_open_paren:
+    inc rcx
+    jmp lexer_scan
+lexer_close_paren:
+    inc rcx
+    jmp lexer_scan
+lexer_done:
+    ret
