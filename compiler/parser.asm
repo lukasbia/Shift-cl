c0fd835793758aed7474969c2f16ac371392af2e
lukasbia <stodomingokylijahnver@gmail.com>
2026-09-10 20:14:15 +0800

    Update parser.asm

--- compiler/parser.asm
+++ compiler/parser.asm
@@ -1,88 +1,64 @@
 section .data
-    kw_variable db "variable", 0
-    kw_constant db "constant", 0
-    kw_func     db "func", 0
-    err_keyword db "Syntax Error: Unknown keyword or invalid structure", 10, 0
-    err_kw_len  equ $ - err_keyword
+    err_syntax db "Parser Error: Unexpected token structure", 10, 0
+    err_syn_len equ $ - err_syntax
 
 section .text
-    global parse_tokens
+    global run_parser
 
-parse_tokens:
+run_parser:
     push rbp
     mov rbp, rsp
     push rbx
     push r12
 
-    mov rsi, token_buffer
+    xor r12, r12
 
 .parse_loop:
-    mov al, byte [rsi]
+    mov al, byte [token_stream + r12]
     cmp al, 0
     je .parse_success
 
-    cmp al, 'v'
-    je .check_variable
+    cmp al, 1
+    je .parse_variable
 
-    cmp al, 'c'
-    je .check_constant
+    cmp al, 2
+    je .parse_constant
 
-    cmp al, 'f'
-    je .check_func
+    cmp al, 3
+    je .parse_func
 
-    inc rsi
+    inc r12
     jmp .parse_loop
 
-.check_variable:
-    mov rdi, kw_variable
-    call match_string
-    cmp rax, 1
+.parse_variable:
+    inc r12
+    mov al, byte [token_stream + r12]
+    cmp al, 99
     jne .syntax_error
+    inc r12
     jmp .parse_loop
 
-.check_constant:
-    mov rdi, kw_constant
-    call match_string
-    cmp rax, 1
+.parse_constant:
+    inc r12
+    mov al, byte [token_stream + r12]
+    cmp al, 99
     jne .syntax_error
+    inc r12
     jmp .parse_loop
 
-.check_func:
-    mov rdi, kw_func
-    call match_string
-    cmp rax, 1
+.parse_func:
+    inc r12
+    mov al, byte [token_stream + r12]
+    cmp al, 99
     jne .syntax_error
+    inc r12
     jmp .parse_loop
-
-.match_string:
-    push rsi
-    push rdi
-.match_loop:
-    mov al, byte [rdi]
-    cmp al, 0
-    je .match_ok
-    mov bl, byte [rsi]
-    cmp al, bl
-    jne .match_fail
-    inc rsi
-    inc rdi
-    jmp .match_loop
-.match_ok:
-    pop rdi
-    pop rsi
-    mov rax, 1
-    ret
-.match_fail:
-    pop rdi
-    pop rsi
-    xor rax, rax
-    ret
 
 .syntax_error:
     mov rax, 1
     mov rdi, 2
-    mov rsi, err_keyword
-    mov rdx, err_kw_len
+    mov rsi, err_syntax
+    mov rdx, err_syn_len
     syscall
 
     mov rax, 60
